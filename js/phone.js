9f55e7098753b27b0dfa5f561077d7db2b89e5bd
Md Noble <mdnoble.wd@gmail.com>
2023-09-08 11:12:07 +0600

    added show all button functionality

--- js/phone.js
+++ js/phone.js
@@ -1,44 +1,47 @@
 // fetch phone data from the api
 
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await res.json();
   const phones = data.data;
   //   console.log(phones);
-  displayPhone(phones);
+  displayPhone(phones, isShowAll);
 };
 
 // showing phone data dynamically
 
-const displayPhone = (phones) => {
-    // console.log(phones);
-
-    // showing show all button when there is more than 12 phones 
-
-    const showAllContainer = document.getElementById('show-all-container');
-
-    if( phones.length > 12) {
-      showAllContainer.classList.remove('hidden');
-    }
-
-    else {
-      showAllContainer.classList.add('hidden');
-    }
-
-    // showing only 12 phones at first 
-
-    phones = phones.slice(0 , 12);
-    // console.log(phones)
+const displayPhone = (phones, isShowAll) => {
+  // console.log(phones);
 
   //   getting the container where I want to display phones
 
   const phoneContainer = document.getElementById("phone-container");
 
-  // clearing the container before adding new phones 
+  // clearing the container before adding new phones
 
-  phoneContainer.textContent = '';
+  phoneContainer.textContent = "";
+
+  // showing show all button when there is more than 12 phones
+
+  const showAllContainer = document.getElementById("show-all-container");
+
+  // console.log('is show all', isShowAll)
+
+  if (phones.length > 12 && !isShowAll) {
+    showAllContainer.classList.remove("hidden");
+  } else {
+    showAllContainer.classList.add("hidden");
+  }
+
+  // console.log('is show all', isShowAll)
+
+  // showing only 12 phones at first if show all button not pressed
+
+  if(!isShowAll){
+    phones = phones.slice(0, 12);
+  }
 
   phones.forEach((phone) => {
     // console.log(phone)
@@ -47,7 +50,7 @@ const displayPhone = (phones) => {
 
     const phoneCard = document.createElement("div");
 
-    // adding classes to the div 
+    // adding classes to the div
 
     phoneCard.classList = `card w-11/12 bg-gray-300 p-8 shadow-xl mx-auto`;
 
@@ -56,59 +59,61 @@ const displayPhone = (phones) => {
     phoneCard.innerHTML = `
     <figure><img src="${phone.image}" /></figure>
     <div class="card-body">
-        <h2 class="card-title">${phone.phone_name}</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div class="card-actions justify-end">
+        <h2 class="card-title mx-auto">${phone.phone_name}</h2>
+        <p class="text-center">If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-center">
         <button class="btn btn-primary">Buy Now</button>
         </div>
     </div>
     `;
 
-    // append the div to the container 
+    // append the div to the container
 
     phoneContainer.appendChild(phoneCard);
   });
 
-  // hiding the loadingSpinner after finishing loading operation of phones 
+  // hiding the loadingSpinner after finishing loading operation of phones
 
   toggleLoadingSpinner(false);
-
 };
 
-// handle search button operation 
+// handle search button operation
 
-const handleButton = () => {
+const handleButton = (isShowAll) => {
   // console.log('search kortase re mama')
 
-  // calling te loadingSpinner function before loading operation is done 
+  // calling te loadingSpinner function before loading operation is done
 
   toggleLoadingSpinner(true);
 
-
-  const searchField = document.getElementById('search-field');
+  const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   // console.log(searchText);
 
-  // calling the load phone function 
+  // calling the load phone function
 
-  loadPhone(searchText);
-  searchField.value = '';
-}
+  loadPhone(searchText, isShowAll);
+  // searchField.value = "";
+};
 
-
-// handle toggle operation of loading or spinner 
+// handle toggle operation of loading or spinner
 
 const toggleLoadingSpinner = (isLoading) => {
-  const loadingSpinner = document.getElementById('loading-spinner');
-  
-  if(isLoading){
-    loadingSpinner.classList.remove('hidden');
-  }
-  else {
-    loadingSpinner.classList.add('hidden');
-  }
+  const loadingSpinner = document.getElementById("loading-spinner");
 
-}
+  if (isLoading) {
+    loadingSpinner.classList.remove("hidden");
+  } else {
+    loadingSpinner.classList.add("hidden");
+  }
+};
+
+// handle show all button
+
+const handleShowAllButton = () => {
+  // console.log('sob dekhabe re mama');
+  handleButton(true);
+};
 // globally calling functions
 
-loadPhone();
+// loadPhone();
